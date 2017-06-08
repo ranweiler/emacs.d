ba245bac27c14ed2323228f85310dd4749c138bb
Joe Ranweiler <josephkennethranweiler@gmail.com>
2017-06-08 15:17:18 -0700

    Add Rust `racer` mode w/`company` integration

--- my/lang-modes.el
+++ my/lang-modes.el
@@ -19,3 +19,11 @@
 (use-package idris-mode :ensure t)
 
 (use-package rust-mode :ensure t)
+
+(use-package racer
+  :ensure t
+  :after company
+  :after rust-mode
+  :config
+  (add-hook 'racer-mode-hook 'company-mode)
+  (add-hook 'rust-mode-hook 'racer-mode))
